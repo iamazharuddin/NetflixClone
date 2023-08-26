e5d736e7a8a18c21653f796efc880833d36ac6c8
iamazharuddin <iamazharuddin10@gmail.com>
2023-08-26 17:40:45 +0530

    basic home page setup

--- NetflixClone/Controllers/DownloadsViewController.swift
+++ NetflixClone/Controllers/DownloadsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DownloadsViewController.swift
 //  NetflixClone
 //
 //  Created by Azharuddin 1 on 26/08/23.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DownloadsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .cyan
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
